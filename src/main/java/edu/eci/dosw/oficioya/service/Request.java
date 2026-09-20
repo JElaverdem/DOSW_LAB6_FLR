dd30c93ce9182223f4bd47be1fe83b4c5b555ef8
BrianFierro03 <56984383+BrianFierro03@users.noreply.github.com>
2026-09-20 00:52:45 -0500

    Create Request.java

---
+++ src/main/java/edu/eci/dosw/oficioya/service/Request.java
@@ -0,0 +1,12 @@
+package edu.eci.dosw.oficioya.service;
+import java.time.LocalDateTime;
+
+public class Request{
+  private LocalDateTime horaPedida;
+  private LocalDateTime horaServicio;
+  private RequestStates estado;
+
+  public Request(){
+
+  }
+}
