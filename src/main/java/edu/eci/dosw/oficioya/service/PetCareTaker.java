dd8ff7025bd7a036fa3c5cab0339368f483cfa36
BrianFierro03 <56984383+BrianFierro03@users.noreply.github.com>
2026-09-20 00:40:10 -0500

    Create PetCareTaker.java

---
+++ src/main/java/edu/eci/dosw/oficioya/service/PetCareTaker.java
@@ -0,0 +1,10 @@
+package edu.eci.dosw.oficioya.service;
+import java.util.ArrayList;
+
+public class PetCareTaker extends Worker{
+  private ArrayList<String> atiende;
+
+  public PetCareTaker(){
+    this.atiende = new ArrayList<>();
+  }
+}
