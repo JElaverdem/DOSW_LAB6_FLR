dc219cf72642f77b1b32d13e52f27c5347cd1f2f
BrianFierro03 <56984383+BrianFierro03@users.noreply.github.com>
2026-09-20 00:23:24 -0500

    Create User.java

---
+++ src/main/java/edu/eci/dosw/oficioya/service/User.java
@@ -0,0 +1,15 @@
+package edu.eci.dosw.oficioya.service;
+
+public class User{
+  private String id;
+  private String name;
+  private String foto;
+  private String correo;
+  private String telefono;
+  private double calificacion;
+  private Suscripcion suscripcion;
+
+  public User(){
+
+  }
+}
